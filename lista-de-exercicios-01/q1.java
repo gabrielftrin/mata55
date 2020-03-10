bc3e7f3609a458bf636b182ed1d79496ebd44805
Gabriel Fernandes Trindade <mrgabrielfernandes@hotmail.com>
2020-03-10 16:14:26 -0300

    Update q1.java

--- lista-de-exercicios-01/q1.java
+++ lista-de-exercicios-01/q1.java
@@ -1,24 +1,9 @@
 import java.util.Date;
-/*
-  
- *  Criar uma classe em que cada objeto representa um voo que acontece em determinada data e em
+/*  
+Criar uma classe em que cada objeto representa um voo que acontece em determinada data e em
 determinado horário. Cada voo possui no máximo 100 passageiros, e a classe permite controlar a
 ocupação das vagas. A classe deve ter os seguintes métodos:
-
- * 
-Método para configurar os dados do voo (recebidos como parâmetro): número do voo, data (para
-armazenar a data utilize um objeto da classe Data, criada na questão anterior);
-
-b) Método para obter o próximo voo livre retornando o número da próxima cadeira livre;
-c) Método para verificar se o número da cadeira recebido como parâmetro está ocupada;
-d) Método para determinar a cadeira do voo ocupada, cujo número é recebido como parâmetro, e
-retorna verdadeiro se a cadeira ainda não estiver ocupada (operação foi bem sucedida) e falso caso
-contrário;
-e) Método para retornar o número de cadeiras vagas disponíveis (não ocupadas) no voo;
-f) Método para retornar o número do voo;
-g) Método para retornar a data do voo.
- * 
- * */
+ */
 public class Voo {
 
 	private int numeroVoo;
@@ -26,32 +11,39 @@ public class Voo {
 	private int hora;
 	private Date data;
 	
+	//Construtor
 	public Voo () {				
 	}
 	
-	//letra a
+	//Método para configurar os dados do voo (recebidos como parâmetro): número do voo, data (para
+	//armazenar a data utilize um objeto da classe Data, criada na questão anterior);
 	public void configurarDadosDoVoo(int numero, Date d) {
 		numeroVoo = numero;
 		data = d;
 	}
 	
-	//letra b
+	//Método para obter o próximo voo livre retornando o número da próxima cadeira livre;
 	public int proximoVoo() {
 		return quantidadePassageiros+1;
 	}
 	
-	//letra c
+	//Método para verificar se o número da cadeira recebido como parâmetro está ocupada;
 	public boolean verificaCadeiraOcupada(int numero) {		
 		if quantidadePassageiros < numero {
 			return false;}		
 		return true;				
 	}
 
-	//letra d
+	//Método para determinar a cadeira do voo ocupada, cujo número é recebido como parâmetro, e
+	//retorna verdadeiro se a cadeira ainda não estiver ocupada (operação foi bem sucedida) e falso caso
+	//contrário;
 	public boolean ocuparCadeira (int numCadeira) {
 				
 	}
 	
-	//letra e
+	//Método para retornar o número de cadeiras vagas disponíveis (não ocupadas) no voo;
 	
+	//Método para retornar o número do voo;
+	
+	//Método para retornar a data do voo.
 }
