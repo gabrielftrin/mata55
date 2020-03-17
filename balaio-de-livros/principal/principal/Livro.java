bdb58ca6ee078fd141f51be80d26aa498d6bca63
Gabriel Fernandes Trindade <mrgabrielfernandes@hotmail.com>
2020-03-17 19:27:00 -0300

    a

---
+++ balaio-de-livros/principal/principal/Livro.java
@@ -0,0 +1,20 @@
+package principal;
+
+public class Livro {
+	
+	String Titulo ; 
+	
+	private String getTitulo() {
+		return Titulo;
+	}
+
+	private void setTitulo(String titulo) {
+		Titulo = titulo;
+	}
+
+	public Livro () {
+		
+		
+	}
+
+}
